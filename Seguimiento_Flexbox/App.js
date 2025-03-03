bacc37f283f1d2e18e807ece3f4003e2170df619
MinPau20 <paula.alvarado0820@gmail.com>
2025-03-03 17:35:47 -0500

    Se actualiza con el código completo

--- Seguimiento_Flexbox/App.js
+++ Seguimiento_Flexbox/App.js
@@ -4,17 +4,70 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box3}></View>
+      
+      <View style={styles.container2}>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+      </View>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: 'flex',
+    backgroundColor: 'pink',
+    alignContent: 'space-around',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
+  box1: {
+    width: 330,
+    height: 200,
+    backgroundColor:'white',
+    borderRadius: 5,
+  },
+  box2: {
+    width: 330,
+    height: 100,
+    backgroundColor:'white',
+    borderRadius: 5,
+  },
+  box3: {
+    width: 90,
+    height: 90,
+    backgroundColor:'white',
+    borderRadius: 5,
+  },
+  container2: {
+    backgroundColor: 'lightblue',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: 330,
+    height: 250,
+    borderRadius: 5,
+  },
+  box: {
+    width: 95,
+    height: 95,
+    marginTop: 30,
+    marginLeft: 11,
+    backgroundColor:'white',
+    borderRadius: 5,
+  }
 });
